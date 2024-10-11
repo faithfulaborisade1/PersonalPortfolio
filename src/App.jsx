e9c02d0e1242d66a6935cc4cf7ece2a977371989
faithful <faithfulaborisade123@gmail.com>
2024-10-11 12:48:15 +0100

    Navbar completed with transitions

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { Navbar } from "./components/navbar/Navbar";
 const App = () => {
 
   return <div>
-    <section>
+    <section id="Homepage">
       <Navbar/>
     </section>
-    <section>Parallax</section>
-    <section>Services</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section>Contact</section>
+    <section id="Services">Parallax</section>
+    <section id="Portfolio">Services</section>
+    <section id="">Portfolio1</section>
+    <section id="">Portfolio2</section>
+    <section id="">Portfolio3</section>
+    <section id="Contact">Contact</section>
   </div>;
 };
 
