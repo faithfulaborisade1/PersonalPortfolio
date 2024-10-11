a0a9381417da4f98d5622f70b27ce9384b613b72
faithful <faithfulaborisade123@gmail.com>
2024-10-11 16:23:49 +0100

    parallax is done fore both sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss"
 import { Navbar } from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
 
@@ -9,11 +10,12 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Services">Parallax</section>
-    <section id="Portfolio">Services</section>
-    <section id="">Portfolio1</section>
-    <section id="">Portfolio2</section>
-    <section id="">Portfolio3</section>
+    <section id="Services"><Parallax type="services"/></section>
+    <section>Services</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
     <section id="Contact">Contact</section>
   </div>;
 };
